0098410247e538fc428ccd9f201a643223d020cd
Ryan <ryan.stewart113@gmail.com>
2022-03-17 23:38:52 -0400

    initial updates for jwt in flask

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,5 +1,11 @@
 from flask import Flask, request, Response
 import socket
+import jwt
+from jwt import PyJWKClient
+
+import logging
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
 
 app = Flask(__name__)
 
@@ -16,3 +22,20 @@ def hello_world():
 def return_headers():
     headers = dict(request.headers)
     return headers
+
+@app.route('/api/testtoken')
+def test_token():
+    headers = dict(request.headers)
+    auth: str = headers.get("Authorization")
+    token = auth.replace("Bearer","").strip()
+   
+    url = "http://keycloak:8080/auth/realms/myrealm/protocol/openid-connect/certs"
+    jwks_client = PyJWKClient(url)
+    signing_key = jwks_client.get_signing_key_from_jwt(token)
+    data = jwt.decode(
+        token,
+        signing_key.key,
+        algorithms=["RS256"],
+        options={"verify_exp": False},
+    )
+    return data
