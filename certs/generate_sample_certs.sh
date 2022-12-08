b6044d11734d2556e85a3239e158402b26d9e51f
rylativity <41017744+rylativity@users.noreply.github.com>
2022-12-08 12:32:04 -0500

    Update generate_sample_certs.sh

--- certs/generate_sample_certs.sh
+++ certs/generate_sample_certs.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-if [ -f ../.env ]; then
-  export $(grep -v '^#' ../.env | xargs)
-fi
-
 #Generate Root Key
 openssl genrsa -out "root-ca.key" 4096
 
@@ -16,7 +12,7 @@ openssl x509 -req -days 3650 -in "root-ca.csr" -signkey "root-ca.key" -sha256 -o
 openssl genrsa -out "site.key" 4096
 
 #Generate the certificate signing request for the site key
-openssl req -new -key "site.key" -out "site.csr" -sha256 -subj "/C=US/ST=Virginia/L=Falls Church/O=MYORG/CN=${FRONTEND_HOSTNAME}"
+openssl req -new -key "site.key" -out "site.csr" -sha256 -subj "/C=US/ST=Virginia/L=Falls Church/O=MYORG/CN=localhost"
 
 #Sign the site certificate (using the "site.cnf" file for config which constrains the certificate so that it can only be used to authenticate a server and not to sign certificates)
 openssl x509 -req -days 750 -in "site.csr" -sha256 -CA "root-ca.crt" -CAkey "root-ca.key" -CAcreateserial -out "site.crt" -extfile "site.cnf" -extensions server
